b30ea362fe6689ef652098067fddc6199aff965b
Anik Roy <anik545@gmail.com>
2020-03-03 18:39:37 +0000

    .

--- single_follower.py
+++ single_follower.py
@@ -10,8 +10,6 @@ import os
 import rospy
 import sys
 
-import matplotlib.pylab as plt
-
 # Robot motion commands:
 # http://docs.ros.org/api/geometry_msgs/html/msg/Twist.html
 from geometry_msgs.msg import Twist
@@ -25,7 +23,11 @@ from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Path
 # For pose information.
 from tf.transformations import euler_from_quaternion
-
+# For converting laser to point cloud
+import laser_geometry.laser_geometry as lg
+# http://docs.ros.org/api/sensor_msgs/html/msg/LaserScan.html
+from sensor_msgs.msg import LaserScan
+import sensor_msgs.point_cloud2 as pc2
 # Import the potential_field.py code rather than copy-pasting.
 directory = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '.')
@@ -43,6 +45,8 @@ EPSILON = .1
 X = 0
 Y = 1
 YAW = 2
+
+ROBOT_NAME = 'tb3_0'
 
 
 def feedback_linearized(pose, velocity, epsilon):
@@ -84,7 +88,7 @@ def get_velocity(position, path_points):
     # MISSING: Return the velocity needed to follow the
     # path defined by path_points. Assume holonomicity of the
     # point located at position.
-    # print(i, v, d)
+    print(i, v, d)
     return cap(dir*np.sqrt(mag), max_speed=SPEED)
 
 
@@ -97,7 +101,7 @@ def cap(v, max_speed):
 
 class SLAM(object):
     def __init__(self):
-        rospy.Subscriber('/map', OccupancyGrid, self.callback)
+        rospy.Subscriber('/'+ROBOT_NAME+'/map', OccupancyGrid, self.callback)
         self._tf = TransformListener()
         self._occupancy_grid = None
         self._pose = np.array([np.nan, np.nan, np.nan], dtype=np.float32)
@@ -117,16 +121,17 @@ class SLAM(object):
     def update(self):
         # Get pose w.r.t. map.
         a = 'occupancy_grid'
-        b = 'tb3_0/base_link'
+        b = ROBOT_NAME+'/base_link'
         if self._tf.frameExists(a) and self._tf.frameExists(b):
             try:
                 t = rospy.Time(0)
                 position, orientation = self._tf.lookupTransform(
                     '/' + a, '/' + b, t)
-                # print(position, orientation)
                 self._pose[X] = position[X]
                 self._pose[Y] = position[Y]
                 _, _, self._pose[YAW] = euler_from_quaternion(orientation)
+                print(self._pose)
+                print(self._occupancy_grid)
             except Exception as e:
                 print(e)
         else:
@@ -207,15 +212,98 @@ def get_path(final_node):
     return zip(points_x, points_y)
 
 
+class SimpleLaser(object):
+    def __init__(self, lp, name=""):
+        rospy.Subscriber('/'+name+'/scan', LaserScan, self.callback)
+        # self._angles = [0., np.pi / 4., -np.pi / 4., np.pi / 2., -np.pi / 2.]
+        n = 100
+        self._angles = np.linspace(-np.pi/2, np.pi/2, n)
+        self._width = np.pi / 180. * 10.  # 10 degrees cone of view.
+        self._measurements = [float('inf')] * len(self._angles)
+        self._indices = None
+        self._lp = lp
+        self._point_gen = None
+
+
+    def callback(self, msg):
+        # print(msg)
+        # Helper for angles.
+        def _within(x, a, b):
+            pi2 = np.pi * 2.
+            x %= pi2
+            a %= pi2
+            b %= pi2
+            if a < b:
+                return a <= x and x <= b
+            return a <= x or x <= b
+        pc2_msg = self._lp.projectLaser(msg)
+        self._point_gen = pc2.read_points(pc2_msg)
+        
+        self._angles = np.arange(
+            msg.angle_min, msg.angle_max, msg.angle_increment)
+        self._measurements = np.array(msg.ranges)
+        # print(msg, self._angles, sself._measurements)
+        # Compute indices the first time.
+        # if self._indices is None:
+        #     self._indices = [[] for _ in range(len(self._angles))]
+        #     for i, d in enumerate(msg.ranges):
+        #         angle = msg.angle_min + i * msg.angle_increment
+        #         for j, center_angle in enumerate(self._angles):
+        #             if _within(angle, center_angle - self._width / 2., center_angle + self._width / 2.):
+        #                 self._indices[j].append(i)
+
+        # ranges = np.array(msg.ranges)
+        # for i, idx in enumerate(self._indices):
+        # We do not take the minimum range of the cone but the 10-th percentile for robustness.
+        # self._measurements[i] = np.percentile(ranges[idx], 10)
+
+    @property
+    def ready(self):
+        return not np.isnan(self._measurements[0])
+
+    @property
+    def measurements(self):
+        return self._measurements
+
+    @property
+    def angles(self):
+        return self._angles
+
+    @property
+    def points(self):
+        return self._point_gen
+
+
+def get_follow_position(pose, laser):
+    cone = np.where((laser.angles > (7*np.pi/4)) | (laser.angles < (np.pi/4)))
+    cone_angles = laser.angles[cone]
+    cone_measures = laser.measurements[cone]
+    close = np.where(cone_measures < 2.5)
+    close_angles = cone_angles[close]
+    closest = np.argmin(cone_measures)
+    closest_dist = cone_measures[closest]
+    closest_angle = cone_angles[closest]
+
+    x = pose[X] + closest_dist * np.cos(closest_angle)
+    y = pose[Y] + closest_dist * np.sin(closest_angle)
+    position = np.array([x, y])
+    print(position)
+    return position
+
+
 def run(args):
     rospy.init_node('rrt_navigation')
 
     # Update control every 100 ms.
     rate_limiter = rospy.Rate(100)
-    publisher = rospy.Publisher('/tb3_0/cmd_vel', Twist, queue_size=5)
-    path_publisher = rospy.Publisher('/tb3_0/path', Path, queue_size=1)
+    publisher = rospy.Publisher('/'+ROBOT_NAME+'/cmd_vel', Twist, queue_size=5)
+    path_publisher = rospy.Publisher(
+        '/'+ROBOT_NAME+'/path', Path, queue_size=1)
     slam = SLAM()
     goal = GoalPose()
+    lp = lg.LaserProjection()
+    laser = SimpleLaser(lp, name=ROBOT_NAME)
+
     frame_id = 0
     current_path = []
     previous_time = rospy.Time.now().to_sec()
@@ -235,11 +323,11 @@ def run(args):
     while not rospy.is_shutdown():
         slam.update()
         current_time = rospy.Time.now().to_sec()
-
         # Make sure all measurements are ready.
         # Get map and current position through SLAM:
         # > roslaunch exercises slam.launch
-        if not goal.ready or not slam.ready:
+        print(goal.ready, slam.ready)
+        if not goal.ready or not slam.ready or not laser.ready:
             rate_limiter.sleep()
             continue
 
@@ -254,16 +342,8 @@ def run(args):
         position = np.array([
             slam.pose[X] + EPSILON * np.cos(slam.pose[YAW]),
             slam.pose[Y] + EPSILON * np.sin(slam.pose[YAW])], dtype=np.float32)
-        print(slam.occupancy_grid.is_occupied(position))
-        print(slam.occupancy_grid.is_free(position))
-        print("robot position: ", position)
-        print(np.argwhere(slam.occupancy_grid._values == 0))
-        print((np.argwhere(slam.occupancy_grid._values == 0)[0]))
-        print(slam.occupancy_grid.get_position(*np.argwhere(slam.occupancy_grid._values == 0)[0]))
-        print(slam.occupancy_grid.get_position(*np.argwhere(slam.occupancy_grid._values == 0)[-1]))
+        print(position)
         v = get_velocity(position, np.array(current_path, dtype=np.float32))
-        slam.occupancy_grid.draw()
-        plt.show()
         u, w = feedback_linearized(slam.pose, v, epsilon=EPSILON)
         vel_msg = Twist()
         vel_msg.linear.x = u
@@ -277,13 +357,13 @@ def run(args):
             continue
         previous_time = current_time
 
+        follow_position = get_follow_position(slam.pose, laser)
         # Run RRT.
         start_node, final_node = rrt.rrt(
-            slam.pose, goal.position, slam.occupancy_grid)
-        print(final_node)
+            slam.pose, follow_position, slam.occupancy_grid)
         current_path = get_path(final_node)
         if not current_path:
-            print('Unable to reach goal position:', goal.position)
+            print('Unable to reach goal position:', follow_position)
 
         # Publish path to RViz.
         path_msg = Path()
